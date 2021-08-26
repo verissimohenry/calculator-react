066ffbd3547404e731dba0eef5f815062493eaad
verissimohenry <verissimohenry04@gmail.com>
2021-08-26 18:17:01 +0100

    Modify app

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <p>Welcome</p>
+      <p>Hello</p>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 // import PropTypes from 'prop-types';
 // import Big from 'big.js';
 
