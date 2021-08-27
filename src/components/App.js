28cc4c99b8e5ca7de509bf11e0f8bbe5c56a00d6
verissimohenry <verissimohenry04@gmail.com>
2021-08-27 16:52:22 +0100

    Modify app.js file

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,9 @@
+import React from 'react';
+import Display from './Display';
+import ButtonPanel from './ButtonPanel';
+import calculate from '../logic/calculate';
+import '../styles/App.css';
+
 export default class App extends React.Component {
   constructor(props) {
     super(props);
