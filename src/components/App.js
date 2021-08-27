5ba38414d5362a6585de68fc6b44022e517a8653
verissimohenry <verissimohenry04@gmail.com>
2021-08-27 14:36:33 +0100

    Modify package json and fix linter error

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import Display from './Display';
+import Display from '../Display';
 import ButtonPanel from './ButtonPanel';
 import '../styles/App.css';
 
 function App() {
   return (
-    <>
       <div className="App">
         <Display />
         <ButtonPanel />
-      </div>
-    </>
+      </div>  
   );
 }
 
