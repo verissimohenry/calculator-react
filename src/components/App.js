bdd7c0bf5612853d2e065a78f75398614437cef1
verissimohenry <verissimohenry04@gmail.com>
2021-08-27 16:51:23 +0100

    Modify App.js file

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,33 @@
-import React from 'react';
-import Display from './Display';
-import ButtonPanel from './ButtonPanel';
-import '../styles/App.css';
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: '0',
+      next: '0',
+      operation: '',
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <Display />
-      <ButtonPanel />
-    </div>
-  );
+  handleClick(buttonName) {
+    const data = calculate(this.state, buttonName);
+    const { total, next, operation } = data;
+    this.setState({
+      total,
+      next,
+      operation,
+    });
+  }
+
+  render() {
+    const { total } = this.state;
+    return (
+      <>
+        <div className="App">
+          <Display result={total} />
+          <ButtonPanel clickHandler={this.handleClick} />
+        </div>
+      </>
+    );
+  }
 }
-
-export default App;
